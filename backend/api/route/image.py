e57f49e00a45cf2307c719e17defbff6cecdec4f
Tommy Nguyen <tommynguyen.164@gmail.com>
2021-11-08 16:20:02 -0800

    Edit image api to return URL to iamge

--- backend/api/route/image.py
+++ backend/api/route/image.py
@@ -1,30 +1,26 @@
 from flask_restful import Resource, reqparse
 from flask import request
 from api.service.s3 import S3_Helper
+import uuid
 import werkzeug
 
 
 class Image(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument(
-        "name",
-        type=str,
-        required=True,
-        help="This field cannot be left blank",
-    )
     s3 = S3_Helper()
 
     def post(self):
-        data = self.parser.parse_args()
         try:
             file_name = request.files["image"]
+            id = str(uuid.uuid4())
             self.s3.upload_fileobj(
                 bucket_name="image-uploader-db",
-                key=data["name"],
+                key=id,
                 data=file_name,
                 ExtraArgs={"ContentType": "image/jpeg"},
             )
-            return {"message": "Image successfully uploaded"}, 200
+            image_url = self.s3.get_fileobj(bucket_name="image-uploader-db", key=id)
+            return {"data": {"url": image_url}}, 201
         except Exception as e:
             print("LOG: {}".format(e))
             return {"message": "Failed to upload image"}, 500
