5b75118109410b5095674b782969b169f23485c3
Tommy Nguyen <tommynguyen.164@gmail.com>
2021-11-08 23:23:52 -0800

    Move backend to root repo

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ from flask_restful import Api
 from api.route.image import Image
 from api.route.image_by_name import ImageByName
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 api = Api(app)
 
 api.add_resource(Image, "/image")
 api.add_resource(ImageByName, "/image/<string:name>")
 
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run()
